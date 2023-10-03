b3d0765d6130bf4ffc9f050b7f0e10bb94bef3a3
RickyLi11228 <124058517+RickyLi11228@users.noreply.github.com>
2023-10-03 10:11:52 -0400

    strat
    start

---
+++ app.js
@@ -0,0 +1,8 @@
+const Feet = Number(prompt("Enter your height in feet."))
+const Inches = Number(prompt("Enter the inches."))
+const Weight = Number(prompt("Enter weight in pounds."))
+function Find_inches(Feet, Inches){
+    console.log((Feet * 12) + Inches)
+}
+Find_inches(Feet, Inches) = Height
+Height(Feet, Inches)
