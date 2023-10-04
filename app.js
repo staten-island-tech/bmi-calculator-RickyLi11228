ce3c160e5e9477e5cec97818e5145ce90371329b
RickyLi11228 <124058517+RickyLi11228@users.noreply.github.com>
2023-10-04 10:13:43 -0400

    IDK
    IDK

--- app.js
+++ app.js
@@ -1,8 +1,14 @@
 const Feet = Number(prompt("Enter your height in feet."))
 const Inches = Number(prompt("Enter the inches."))
 const Weight = Number(prompt("Enter weight in pounds."))
-function Find_inches(Feet, Inches){
-    console.log((Feet * 12) + Inches)
+function calc_inches(Feet, Inches, poop){
+    console.log((Feet * 12) + Inches) = poop
 }
-Find_inches(Feet, Inches) = Height
-Height(Feet, Inches)
+calc_inches(Feet, Inches) 
+function calc_bmi(Weight, poop) {
+   console.log(Weight / poop)
+}
+calc_bmi(Weight, poop)
+
+
+
