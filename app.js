cc3e7330efbef90179f40478335389a2fcc468b4
RickyLi11228 <124058517+RickyLi11228@users.noreply.github.com>
2023-10-05 09:32:15 -0400

    ez
    ze

--- app.js
+++ app.js
@@ -1,14 +1,16 @@
-const Feet = Number(prompt("Enter your height in feet."))
-const Inches = Number(prompt("Enter the inches."))
+const Height = Number(prompt("Enter your height in inches."))
 const Weight = Number(prompt("Enter weight in pounds."))
-function calc_inches(Feet, Inches, poop){
-    console.log((Feet * 12) + Inches) = poop
+const step1 = Height * Height
+const step2 = Weight / step1
+const BMI = step2 * 703
+console.log("Your BMI is:")
+console.log(BMI)
+if (BMI <18.5){
+console.log("You are underweight")
+} else if (BMI >18.5 && BMI <24.9) {
+    console.log("You are normal")
+} else if (BMI >25  && BMI <29.9) {
+    console.log("You are overweight")
+} else if (BMI >30) {
+    console.log("You are obese")
 }
-calc_inches(Feet, Inches) 
-function calc_bmi(Weight, poop) {
-   console.log(Weight / poop)
-}
-calc_bmi(Weight, poop)
-
-
-
